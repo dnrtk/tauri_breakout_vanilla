412d0f2d9fec2040b2253924c1def7e2ae2d1156
dnrtk <asaz.dnrtk@gmail.com>
2022-08-18 10:13:52 +0900

    ウィンドウ関連の設定 カウントAPIをRust側に移行

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,15 @@
   windows_subsystem = "windows"
 )]
 
+#[tauri::command]
+fn count_api(counter: i32) -> i32 {
+  println!("count_api: {}", counter);
+  return counter + 1;
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![count_api])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/counter.ts
+++ src/counter.ts
@@ -1,9 +1,20 @@
+import { invoke } from '@tauri-apps/api/tauri';
+
 export function setupCounter(element: HTMLButtonElement) {
   let counter = 0
   const setCounter = (count: number) => {
     counter = count
     element.innerHTML = `count is ${counter}`
   }
-  element.addEventListener('click', () => setCounter(++counter))
+
+  element.addEventListener('click', () => {
+    invoke('count_api', {counter}).then(res => {
+      console.log(res);
+      setCounter(res as number);
+    }).catch(e => {
+      console.error(e);
+    })
+  })
+
   setCounter(0)
 }
