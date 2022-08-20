e991924da47a7874419f7e7f778cff25a8362b5c
dnrtk <asaz.dnrtk@gmail.com>
2022-08-20 14:07:16 +0900

    Fieldのパラメータ構造体を定義

--- src/field.ts
+++ src/field.ts
@@ -15,9 +15,29 @@ enum GAME_STATUS {
   GAME_CLEAR = 3
 };
 
+interface Pos2 {
+  x: number,
+  y: number
+};
+
+interface FieldStatus {
+  pos_min: Pos2,
+  pos_max: Pos2,
+  color: String,
+  border_color: String,
+  game_status: GAME_STATUS
+};
+
 class Field {
   ctx: CanvasRenderingContext2D;
-  game_status = GAME_STATUS.PLAYING;
+  status: FieldStatus = {
+    pos_min: {x: FIELD_START_POS_X_INIT, y: FIELD_START_POS_Y_INIT},
+    pos_max: {x: FIELD_START_POS_X_INIT + FIELD_WIDTH, y: FIELD_START_POS_Y_INIT + FIELD_HEIGHT},
+    color: FIELD_FILL_STYLE,
+    border_color: FIELD_STROKE_STYLE,
+    game_status: GAME_STATUS.PLAYING
+  };
+
 
   constructor(ctx: CanvasRenderingContext2D) {
     this.ctx = ctx;
@@ -28,14 +48,13 @@ class Field {
     // 描画(フィールド)
     this.ctx.beginPath();
     this.ctx.rect(FIELD_START_POS_X_INIT, FIELD_START_POS_Y_INIT, FIELD_WIDTH, FIELD_HEIGHT);
-    // this.ctx.rect(0, 0, 510, 610);
     this.ctx.fillStyle = FIELD_FILL_STYLE;
     this.ctx.fill();
     this.ctx.strokeStyle = FIELD_STROKE_STYLE;
     this.ctx.lineWidth = FIELD_LINE_WIDTH;
     this.ctx.stroke();
 
-    switch(this.game_status) {
+    switch(this.status.game_status) {
       case GAME_STATUS.GAME_OVER:
         this.drawGameOver();
         break;
@@ -62,7 +81,7 @@ class Field {
   }
 
   setGameStatus(game_status: GAME_STATUS) {
-    this.game_status = game_status;
+    this.status.game_status = game_status;
   }
 }
 
